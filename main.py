be1f8fbead6eafa6dec81e20b2822b7d388ee181
Blake-Evelyn Mancill <kmanci6@wgu.edu>
2025-09-25 18:37:20 -0400

    updated nearest neighbor algoirthm to be deadline aware

--- main.py
+++ main.py
@@ -97,43 +97,40 @@ truck3 = Truck(16, 18, None, [2, 4, 5, 6, 7, 8, 9, 10, 11, 25, 28, 32, 33], 0.0,
 # Load packages into hash table
 load_package_data("data/packages.csv", package_hash_table)
 
-# Orders packages on a given truck using the nearest neighbor algorithm.
-# Calculates the distance a truck drives once the package are sorted
 def nearest_neighbor(truck):
-    # Place all packages into array of not delivered
     not_delivered = []
     truck.time = truck.depart_time
     for package_id in truck.packages:
         package = package_hash_table.lookup(package_id)
         not_delivered.append(package)
 
-    # Clear the package list of a given truck so the packages can be placed in optimal order
     truck.packages.clear()
 
-    # Updates address for package 9 if truck leaves at or after 10:20. Removed due to bug where package address
-    # would update before 10:20
-#    if truck.depart_time >= datetime.timedelta(hours=10, minutes=20):
-#        update_package_9_address()
-
-    # Cycle through the list of not_delivered until none remain in the list
-    # Adds the nearest package into the truck.packages list one by one
     while len(not_delivered) > 0:
-        next_address = float('inf')
         next_package = None
+        next_address_dist = float('inf')
+
+        # Prioritize by earliest deadline, then by shortest distance
+        # Sort not_delivered by deadline (earliest first), then by distance from current truck location
+        current_address_index = extract_address(truck.address)
+        not_delivered.sort(key=lambda p: (
+            p.deadline_time,  # Primary: earliest deadline first
+            distance_between(current_address_index, extract_address(p.address))  # Secondary: shortest distance
+        ))
+
+        # Now pick the first eligible package (closest among earliest deadlines)
         for package in not_delivered:
-            if distance_between(extract_address(truck.address), extract_address(package.address)) <= next_address:
-                next_address = distance_between(extract_address(truck.address), extract_address(package.address))
-                next_package = package
-        # Adds next closest package to the truck package list
+            dist = distance_between(current_address_index, extract_address(package.address))
+            next_package = package
+            next_address_dist = dist
+            break
+
+        # Update truck and package
         truck.packages.append(next_package.package_id)
-        # Removes the same package from the not_delivered list
         not_delivered.remove(next_package)
-        # Takes the mileage driven to this packaged into the truck.mileage attribute
-        truck.mileage += next_address
-        # Updates truck's current address attribute to the package it drove to
+        truck.mileage += next_address_dist
         truck.address = next_package.address
-        # Updates the time it took for the truck to drive to the nearest package
-        truck.time += datetime.timedelta(hours=next_address / 18)
+        truck.time += datetime.timedelta(hours=next_address_dist / 18)
         next_package.delivery_time = truck.time
         next_package.departure_time = truck.depart_time
 
