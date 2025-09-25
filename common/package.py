5364f74544f82bd4fde8e42f17df28f23c674f83
Blake-Evelyn Mancill <kmanci6@wgu.edu>
2025-09-24 21:15:32 -0400

    implemented package class

---
+++ common/package.py
@@ -0,0 +1,26 @@
+
+class Package:
+    def __init__(self, id, address, city, state, zipcode, deadline_time, weight, status, departure_time, delivery_time):
+        self.id = id
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+        self.deadline_time = deadline_time
+        self.weight = weight
+        self.status = status
+        self.departure_time = departure_time
+        self.delivery_time = delivery_time
+
+    def __str__(self):
+        return "%s, %s, %s, %s, %s, %s, %s, %s, %s" % (self.id, self.address, self.city, self.state, self.zipcode,
+                                                       self.deadline_time, self.weight, self.delivery_time,
+                                                       self.status)
+
+    def update_status(self, convert_timedelta):
+        if self.delivery_time < convert_timedelta:
+            self.status = "Delivered"
+        elif self.delivery_time > convert_timedelta:
+            self.status = "En route"
+        else:
+            self.status = "At Hub"
